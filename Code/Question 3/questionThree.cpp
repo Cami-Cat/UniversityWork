988378717690b31e600d22b9267ab6df34d026d3
Cami-Cat <katten.en.vt@gmail.com>
2024-09-27 18:40:05 +0100

    Update questionThree.cpp

--- Code/Question 3/questionThree.cpp
+++ Code/Question 3/questionThree.cpp
@@ -4,8 +4,7 @@ using namespace std;
 void main() {
 
 int result = (6 % 4);           // the remainder is 2 because 6/4 = 1 (As a whole number) and thus calculates 6-4 to get the remaining two numbers. 
-                                //It is typically unnecessary to use a float || double to store the result of a modulo operation unless one or both inputs are floats || doubles.
-int resultTwo = (1252 % 42);
+int resultTwo = (1252 % 42);    //It is typically unnecessary to use a float || double to store the result of a modulo operation unless one or both inputs are floats || doubles.
 
     cout << "Modulo (Modulus || %) is a mathematical operator that calculates the remainder of a division between two numbers. The equation 6 % 4 would be equal to: " << result << "\nJust as the remainder of the equation 1252 % 42 would be: " << resultTwo << endl;
     return;
