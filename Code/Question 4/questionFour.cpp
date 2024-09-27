6e62fefc497d662a29177e793929350589c4f79c
Cami-Cat <katten.en.vt@gmail.com>
2024-09-27 18:43:03 +0100

    Appending comments
    making it easier to understand the thought process

--- Code/Question 4/questionFour.cpp
+++ Code/Question 4/questionFour.cpp
@@ -3,21 +3,33 @@ using namespace std;
 
 void main(){
 
+// only allowed two variables to calculate four integers. First, set the variables
+
 int input{0};
 int result{0};
 
+
+        // Collect the first input.
         cout << "Write four integers" << endl;
         cin >> input;
+        // Add this input to the result.
         result += input;
         cin >> input;
+        // Add this input to the result, now input A and input B have been stored as a+b in result.
         result += input;
         cin >> input;
+        // Now input A, B and C are being stored as a+b+c.
         result += input;
         cin >> input;
+        // a+b+c+d
         result += input;
 
+
+        // Divide the result by the number specified, 4
         result = result / 4;
 
-        cout << "The average of the chosen integers is (less accurate than float): " << result << endl;
 
+        // Print result
+        cout << "The average of the chosen integers is (less accurate than float): " << result << endl;
+        return;
 }
